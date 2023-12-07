9f6996909a7fbbc5f2301e2f1389cc21203712db
TarunHokage <saikalyantarun2001@gmail.com>
2023-12-08 00:10:11 +0530

    WIP Slider_JS

--- Slider_JS/js/main.js
+++ Slider_JS/js/main.js
@@ -3,12 +3,14 @@ const inpBlur = document.getElementById('blur');
 const inpColor = document.getElementById('clor');
 
 
-const clor = inpColor.addEventListener('mouseover', changeColor);
+const clor = inpColor.addEventListener('mousemove', changeColor);
 const setlor = inpColor.addEventListener('change', setColor);
-const space = inpSpace.addEventListener('mouseover', changeSpace);
+// const space = inpSpace.addEventListener('mousemove', changeSpace);
 const setpace = inpSpace.addEventListener('change', setSpace);
-const blur = inpBlur.addEventListener('mouseover', changeBlur);
+// const blur = inpBlur.addEventListener('mousemove', changeBlur);
 const setlur = inpBlur.addEventListener('change', setBlur);
+
+
 
 function changeColor(e){
     console.log(e);
@@ -18,17 +20,17 @@ function setColor(e){
     console.log(e);
 }
 
-function changeSpace(e){
-    console.log(e);
-}
+// function changeSpace(e){
+//     console.log(e);
+// }
 
 function setSpace(e){
     console.log(e);
 }
 
-function changeBlur(e){
-    console.log(e);
-}
+// function changeBlur(e){
+//     console.log(e);
+// }
 
 function setBlur(e){
     console.log(e);
