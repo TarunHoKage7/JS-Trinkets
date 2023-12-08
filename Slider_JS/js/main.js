7b74ac079f968416efc72ce14944828c4045394b
TarunHokage <saikalyantarun2001@gmail.com>
2023-12-08 12:34:22 +0530

    Slider Working

--- Slider_JS/js/main.js
+++ Slider_JS/js/main.js
@@ -1,37 +1,24 @@
 const inpSpace = document.getElementById('spacing');
 const inpBlur = document.getElementById('blur');
 const inpColor = document.getElementById('clor');
+const root_theme = document.querySelector(':root');
 
 
-const clor = inpColor.addEventListener('mousemove', changeColor);
-const setlor = inpColor.addEventListener('change', setColor);
-// const space = inpSpace.addEventListener('mousemove', changeSpace);
-const setpace = inpSpace.addEventListener('change', setSpace);
-// const blur = inpBlur.addEventListener('mousemove', changeBlur);
-const setlur = inpBlur.addEventListener('change', setBlur);
+const clor = inpColor.addEventListener('mousemove', setColor);
+const space = inpSpace.addEventListener('mousemove', setSpace);
+const blur = inpBlur.addEventListener('mousemove', setBlur);
 
-
-
-function changeColor(e){
-    console.log(e);
-}
 
 function setColor(e){
-    console.log(e);
+    root_theme.style.setProperty('--shaded-background-color', e.target.value);
 }
 
-// function changeSpace(e){
-//     console.log(e);
-// }
 
 function setSpace(e){
-    console.log(e);
+    root_theme.style.setProperty('--spacing', `${e.target.value}px`);
 }
 
-// function changeBlur(e){
-//     console.log(e);
-// }
 
 function setBlur(e){
-    console.log(e);
+    root_theme.style.setProperty('--blur', `${e.target.value}px`);
 }
